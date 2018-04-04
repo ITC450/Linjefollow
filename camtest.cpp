ac5f1f3a75fea5d825fc77966f773dd500683987
Jeppe Nellemann <jepnel@gmail.com>
2018-04-04 10:57:52 +0200

    test

---
+++ camtest.cpp
@@ -0,0 +1,32 @@
+#include <opencv2/opencv.hpp>
+
+
+
+int main()
+{
+        VideoCapture stream1(0);
+        if(!stream1.isOpened()) {
+                std::cerr << "cannot open camera" << std::endl;
+                return -1;
+        }
+        Mat cameraFrame;
+        stream1 >> cameraFrame;
+        while (true) {
+
+                stream1 >> cameraFrame;
+                //Check if feed has stopped
+                if(cameraFrame.empty()) {
+                        std::cerr<<"the frame is empty"<<std::endl;
+                        break;
+                }
+                namedWindow( "Frame", CV_WINDOW_AUTOSIZE );
+                imshow("Frame", cameraFrame);
+                char c=(char)waitKey(1);
+                if(c==27)
+                        break;
+        }
+        //Clean up
+        stream1.release();
+        destroyAllWindows();
+        return 0;
+}
