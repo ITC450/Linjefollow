668620fcc3c81bd850b75167fb735ff33a86a06a
Jeppe Nellemann <jepnel@gmail.com>
2018-04-26 10:27:58 +0200

    draw arrow

--- motor.cpp
+++ motor.cpp
@@ -34,11 +34,11 @@ void RightMotor(direction dir, int speed, Mat mat, int rows, int cols) {
     {
         digitalWrite(25,HIGH);
         digitalWrite(24,LOW);
-        //arrowedLine(mat, Point2f((cols/8)*7,rows/2), Point2f((cols/8)*7,(rows/2)+speed), Scalar(0,255-speed,speed), 1, 8, 0);
+        arrowedLine(mat, Point2f((cols/8)*7,rows/2), Point2f((cols/8)*7,(rows/2)+speed), Scalar(0,255-speed,speed), 1, 8, 0);
     }else{
         digitalWrite(25,LOW);
         digitalWrite(24,HIGH);
-        //arrowedLine(mat, Point2f((cols/8)*7,rows/2), Point2f((cols/8)*7,(rows/2)-speed), Scalar(0,255-speed,speed), 1, 8, 0);
+        arrowedLine(mat, Point2f((cols/8)*7,rows/2), Point2f((cols/8)*7,(rows/2)-speed), Scalar(0,255-speed,speed), 1, 8, 0);
     }
     pwmWrite(23,speed);
 }
