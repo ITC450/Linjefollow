043153d6ffc4194e5692f579b727b25e5f1d7a6c
Jeppe Nellemann <jepnel@gmail.com>
2018-04-26 13:57:05 +0200

    pid first commit

--- motor.cpp
+++ motor.cpp
@@ -43,6 +43,32 @@ void RightMotor(direction dir, int speed, Mat mat, int rows, int cols) {
     pwmWrite(23,speed);
 }
 
+double integral = 0;
+std::chrono::time_point start;
+//std::chrono::time_point end;
+int last_err = 0;
+
+double pid(int err) {
+    err -= 14;
+
+
+    double Pout = kp * err; // P delen udregnes
+    auto end = std::chrono::high_resolution_clock::now();
+    auto result = std::chrono::duration_cast<std::chrono::microseconds>(end-start);
+    integral += (result.count() * err);  // I delen udregnes
+    start = std::chrono::high_resolution_clock::now();
+    double Iout = ki * integral;
+    double derivative = (err - last_err);
+    double Dout = kd * derivative;
+
+    double output = Pout + Iout + Dout;
+    if (output > 450)output = 450; // sørger for output holder sig inden for range
+    else if (output < 50)output = 50; // myPID.Compute();
+
+    last_err=err;
+    return output;
+}
+
 //Follow line function
 void MotorFollowLine(int err, Mat mat, int rows, int cols, int speed){
     double error = err * 0.5;
