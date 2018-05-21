61976700f96fe8d5caf97c951210dc6916426c3a
Line <lvni16@student.aau.dk>
2018-05-21 12:35:31 +0200

    PD der virker

--- motor.cpp
+++ motor.cpp
@@ -1,99 +1,86 @@
 #include "motor.hpp"
 
-void MotorInit()
-{
-   //std::cout << "Setting up WiringPi" << '\n';
-   if (wiringPiSetup() == -1)
-   {
-      //std::cout << "WiringPi setup failed" << '\n';
-      exit(1);
-   }
-   pinMode(1, PWM_OUTPUT);
-   pinMode(23, PWM_OUTPUT);
-   pinMode(2, OUTPUT);
-   pinMode(3, OUTPUT);
-   pinMode(24, OUTPUT);
-   pinMode(25, OUTPUT);
+void MotorInit() {
+    std::cout << "Setting up WiringPi" << '\n';
+    if (wiringPiSetup() == -1)
+    {
+        std::cout << "WiringPi setup failed" << '\n';
+        exit(1);
+    }
+    pinMode(1, PWM_OUTPUT);
+    pinMode(23, PWM_OUTPUT);
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(24, OUTPUT);
+    pinMode(25, OUTPUT);
 }
 
-void LeftMotor(direction dir, int speed, Mat mat, int rows, int cols)
-{
-   if (dir == BACK)
-   {
-      digitalWrite(2, HIGH);
-      digitalWrite(3, LOW);
-      arrowedLine(mat, Point2f(cols / 8, rows / 2), Point2f(cols / 8, (rows / 2) + speed), Scalar(0, 255 - speed, speed), 1, 8, 0);
-   }
-   else
-   {
-      digitalWrite(2, LOW);
-      digitalWrite(3, HIGH);
-      arrowedLine(mat, Point2f(cols / 8, rows / 2), Point2f(cols / 8, (rows / 2) - speed), Scalar(0, 255 - speed, speed), 1, 8, 0);
-   }
-   pwmWrite(1, speed);
+void LeftMotor(direction dir, int speed, Mat mat, int rows, int cols) {
+    if (dir == BACK)
+    {
+        digitalWrite(2,HIGH);
+        digitalWrite(3,LOW);
+        arrowedLine(mat, Point2f(cols/8,rows/2),     Point2f(cols/8,    (rows/2)+speed), Scalar(0,255-speed,speed), 1, 8, 0);
+    }else{
+        digitalWrite(2,LOW);
+        digitalWrite(3,HIGH);
+        arrowedLine(mat, Point2f(cols/8,rows/2),     Point2f(cols/8,    (rows/2)-speed), Scalar(0,255-speed,speed), 1, 8, 0);
+    }
+    pwmWrite(1,speed);
 }
 
-void RightMotor(direction dir, int speed, Mat mat, int rows, int cols)
-{
-   if (dir == BACK)
-   {
-      digitalWrite(25, HIGH);
-      digitalWrite(24, LOW);
-      arrowedLine(mat, Point2f((cols / 8) * 7, rows / 2), Point2f((cols / 8) * 7, (rows / 2) + speed), Scalar(0, 255 - speed, speed), 1, 8, 0);
-   }
-   else
-   {
-      digitalWrite(25, LOW);
-      digitalWrite(24, HIGH);
-      arrowedLine(mat, Point2f((cols / 8) * 7, rows / 2), Point2f((cols / 8) * 7, (rows / 2) - speed), Scalar(0, 255 - speed, speed), 1, 8, 0);
-   }
-   pwmWrite(23, speed);
+void RightMotor(direction dir, int speed, Mat mat, int rows, int cols) {
+    if (dir == BACK)
+    {
+        digitalWrite(25,HIGH);
+        digitalWrite(24,LOW);
+        arrowedLine(mat, Point2f((cols/8)*7,rows/2), Point2f((cols/8)*7,(rows/2)+speed), Scalar(0,255-speed,speed), 1, 8, 0);
+    }else{
+        digitalWrite(25,LOW);
+        digitalWrite(24,HIGH);
+        arrowedLine(mat, Point2f((cols/8)*7,rows/2), Point2f((cols/8)*7,(rows/2)-speed), Scalar(0,255-speed,speed), 1, 8, 0);
+    }
+    pwmWrite(23,speed);
 }
 
-double inte     = 0;
-int    last_err = 0;
+double inte = 0;
+int last_err = 0;
 
-double pid(int err, std::chrono::time_point <std::chrono::high_resolution_clock>&pid_start)
-{
-   err -= 8;
-   auto pid_end = std::chrono::system_clock::now();
-   std::chrono::duration <double, std::milli> dur = pid_start - pid_end;
-   pid_start = std::chrono::system_clock::now();
-   double Pout = kp * err;    // P delen udregnes
+double pid(int err, std::chrono::time_point<std::chrono::high_resolution_clock> &pid_start) {
+    err -= 8;
+    auto pid_end = std::chrono::system_clock::now();
+    std::chrono::duration<double, std::milli> dur = pid_end - pid_start;
+    pid_start = std::chrono::system_clock::now();
+    double Pout = kp * err; // P delen udregnes
+    inte +=  err * (dur.count()/1000);  // I delen udregnes
+    double Iout = ki * inte;
+    double derivative = (err - last_err)/(dur.count()/1000);
+    double Dout = kd * derivative;
 
-   inte += err * dur.count(); // I delen udregnes
+    double output = Pout + Iout+ Dout;
 
-   double Iout       = ki * inte;
-   double derivative = (err - last_err) / dur.count();
-   double Dout       = kd * derivative;
-
-   double output = Pout + Dout;  //+ Iout;
-
-   last_err = err;
-   return(output);
+    last_err=err;
+    return output;
 }
 
 //Follow line function
-void MotorFollowLine(int err, Mat mat, int rows, int cols, int speed, std::chrono::time_point <std::chrono::high_resolution_clock>&pid_start)
-{
-   double error = pid(err, pid_start);
-
-   if (err < 0)
-   {
-      LeftMotor(FORWARD, speed - int(abs(error)), mat, rows, cols);
-      RightMotor(FORWARD, speed + int(abs(error)), mat, rows, cols);
-      return;
-   }
-   if (err > 0)
-   {
-      RightMotor(FORWARD, speed - int(abs(error)), mat, rows, cols);
-      LeftMotor(FORWARD, speed + int(abs(error)), mat, rows, cols);
-      return;
-   }
-   if (err == 0)
-   {
-      RightMotor(FORWARD, speed, mat, rows, cols);
-      LeftMotor(FORWARD, speed, mat, rows, cols);
-      return;
-   }
+void MotorFollowLine(int err, Mat mat, int rows, int cols, int speed, std::chrono::time_point<std::chrono::high_resolution_clock> &pid_start){
+    double error = pid(err, pid_start);
+    error=abs(error)+1;
+    std::cout << error << "\n";
+    if(err < 0) {
+        RightMotor(FORWARD, ((speed*2)/(error+1))*error, mat, rows, cols);
+        LeftMotor(FORWARD, (speed*2)/(error+1), mat, rows, cols);
+        return;
+    }
+    if(err > 0) {
+        LeftMotor(FORWARD, ((speed*2)/(error+1))*error, mat, rows, cols);
+        RightMotor(FORWARD, (speed*2)/(error+1), mat, rows, cols);
+        return;
+    }
+    if(err == 0) {
+        RightMotor(FORWARD, speed, mat, rows, cols);
+        LeftMotor(FORWARD, speed, mat, rows, cols);
+        return;
+    }
 }
